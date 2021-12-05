34cf98569daa6a3446231a201994e1f5ba4f39a5
vishalvishalgupta <vishal.vishal.gupta48@gmail.com>
2021-12-05 22:54:57 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import StarComponent from './StarComponent'
+function App() {
+  return (
+    <div className="App">
+      <StarComponent />
+    </div>
+  );
+}
+
+export default App;
